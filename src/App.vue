7eee561055237b79540bcad23ce662049288e487
gyk419 <gyk419@gmail.com>
2024-03-10 21:27:54 +0900

    240310 권지용 yona로 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/background.png">
-  <div class="rounded-box"></div>
+  <div class="main">
+    <img class="background-image" alt="background" src="./assets/background.png">
+    <div class="rounded-box">
+      <div class="main-box">
+        <h1>Meet Point</h1>
+        <h3 style="color: lightgray;">이름과 위치를 입력하고 원하는 방식으로 중간지점을 검색하세요!</h3>
+        <div class="info-box">
+          <section>s</section>
+          <section>d</section>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,35 +24,54 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main {
+  width: 100%;
+  height: 100%;
 }
+
 .background-image {
   width: 100%;
-  height: auto;
-  position: absolute; /* 이미지를 절대적으로 설정 */
+  height: 100%;
+  position: absolute;
   top: 0;
   left: 0;
-  z-index: 1; /* 이미지를 맨 위로 올리기 */
+  z-index: 1;
 }
 
 .rounded-box {
-  width: 500px;
-  height: 500px;
-  background-color: rgba(169, 169, 169, 0.5);
+  width: 65%;
+  height: 85%;
+  background-color: rgba(0, 0, 0, 0.5);
   border-radius: 1%;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: white;
-  position: absolute; /* 박스를 절대적으로 설정 */
-  top: 50px; /* 원하는 위치로 조절 */
-  left: 50px; /* 원하는 위치로 조절 */
-  z-index: 2; /* 박스를 이미지 위로 올리기 */
+  position: absolute;
+  top: 7%;
+  left: 17%;
+  z-index: 2;
 }
+
+.main-box {
+  width: 98%;
+  height: 97%;
+  background-color: white;
+  border-radius: 1%;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: center;
+  position: absolute;
+  z-index: 3;
+}
+
+.info-box{
+  width: 90%;
+  height: 70%;
+  background: linear-gradient(to right, #cdffd8 30%, #94b9ff);
+  border-radius: 1%;
+  z-index: 4;
+  display: flex;
+}
+
 </style>
