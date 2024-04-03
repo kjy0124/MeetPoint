fe30d5eef5a37b207f5bcaca0a059508b1055658
KimJunSik <djdj388@cu.ac.kr>
2024-04-03 16:23:30 +0900

    240403 김준식 axios  설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // outputDir: "../src/main/resource/static",
+  devServer: {
+    proxy: {
+      '/map': {
+        // /map로 들어오면 포트 8080(스프링 서버)로 보냄
+        target: 'http://localhost:8080',
+        pathRewrite:{'^/':''},
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  },
+  transpileDependencies: true,
+
 })
