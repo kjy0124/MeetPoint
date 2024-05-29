29ad20f7e7f3c7bfb73741590d7faddc4b13c11e
gyk419 <gyk419@gmail.com>
2024-05-29 09:49:46 +0900

    자질구래 권지용

--- src/components/MiddleMap.vue
+++ src/components/MiddleMap.vue
@@ -157,7 +157,6 @@ export default {
 
             const imageSrc = require('@/assets/중간지점.png');
             const imageSize = new kakao.maps.Size(50, 50);
-            // const imageOption =  { offset: new kakao.maps.Point(27, 69) }
             const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
             //중간 지점 마커 생성
@@ -295,7 +294,6 @@ export default {
                 const rand0_5 = Math.floor(Math.random()* 6)
                 var imageSrc = require('@/assets/human'+rand0_5+'.png'), // 마커이미지의 주소입니다    
                     imageSize = new kakao.maps.Size(50, 50) // 마커이미지의 크기입니다
-                    // imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
                 var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
 
